8f1b403d6ce5cab17d7722c8380b7a5affa39830
Damian Krzeminski <krzemdamian@gmail.com>
2018-11-19 22:05:03 +0100

    working: dynamic class emiter

---
+++ DKTools/RevitDKTools/Command/Receiver/DynamicClassEmiter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitDKTools.Command.Receiver
+{
+    static class DynamicClassEmiter
+    {
+        static void Create()
+        {
+            //create assembly
+            AssemblyName an = new AssemblyName();
+            an.Name = "DynamicAssembly";
+            AppDomain ad = AppDomain.CurrentDomain;
+            AssemblyBuilder ab = ad.DefineDynamicAssembly(an,
+                AssemblyBuilderAccess.Save);
+
+            //create module
+            ModuleBuilder mb = ab.DefineDynamicModule(an.Name, an.Name + ".dll");
+        }
+    }
+}
