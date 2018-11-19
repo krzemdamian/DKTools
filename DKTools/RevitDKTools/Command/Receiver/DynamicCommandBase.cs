03bcb3cd1fd6d2cc726a5a93fa51d8a35ad394d0
Damian Krzeminski <krzemdamian@gmail.com>
2018-11-19 19:16:28 +0100

    add DynamiCommandBase
    this will be base class for emited dynamic classes

---
+++ DKTools/RevitDKTools/Command/Receiver/DynamicCommandBase.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitDKTools.Command.Receiver
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class DynamicCommandBase : IExternalCommand
+    {
+        readonly string _scriptPath;
+
+        public DynamicCommandBase(string scriptPath)
+        {
+            _scriptPath = scriptPath;
+        }
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            DKToolsApp.MyPythonEngine.RunScript(
+                _scriptPath,
+                commandData, out message, elements);
+
+            return Result.Succeeded;
+        }
+    }
+}
