a720b6ca49876d204faf5b9c8bdd50f421802ad8
Damian Krzeminski <krzemdamian@gmail.com>
2018-11-22 21:27:25 +0100

    add create type and return type

--- DKTools/RevitDKTools/Command/Receiver/DynamicClassEmiter.cs
+++ DKTools/RevitDKTools/Command/Receiver/DynamicClassEmiter.cs
@@ -30,7 +30,7 @@ namespace RevitDKTools.Command.Receiver
             _moduleBuilder = _assemblyBuilder.DefineDynamicModule(_asseblyName.Name, _asseblyName.Name + ".dll");
         }
 
-        protected void BuildCommandType(string commandTypeName, string scriptPath)
+        protected Type BuildCommandType(string commandTypeName, string scriptPath)
         {
             TypeBuilder typeBuilder = _moduleBuilder.DefineType(
                 commandTypeName, TypeAttributes.Public | TypeAttributes.Class, typeof(DynamicCommandBase));
@@ -51,6 +51,8 @@ namespace RevitDKTools.Command.Receiver
             ilg.Emit(OpCodes.Ldstr, scriptPath);
             ilg.Emit(OpCodes.Stfld, scriptPathField);
             ilg.Emit(OpCodes.Ret);
+
+            return typeBuilder.CreateType();
         }
 
     }
