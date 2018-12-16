66b985da15de3c5e657f9d69331b02a1fb505813
Damian Krzeminski <krzemdamian@gmail.com>
2018-12-16 21:06:47 +0100

    Add assembly metadata

--- DKTools/RevitDKTools/Properties/AssemblyInfo.cs
+++ DKTools/RevitDKTools/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RevitDKTools")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Revit Add-in and ironpython command framework.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RevitDKTools")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018 by Damian Krzemiński, Apache-2.0 licensed")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
