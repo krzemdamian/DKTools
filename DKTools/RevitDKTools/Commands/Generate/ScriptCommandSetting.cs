2e43521dfd513b79258668a0120d935a64ef1fd7
Damian Krzeminski <krzemdamian@gmail.com>
2019-01-12 10:48:26 +0100

    Add ScriptCommandSetting class

---
+++ DKTools/RevitDKTools/Commands/Generate/ScriptCommandSetting.cs
@@ -0,0 +1,11 @@
+﻿namespace RevitDKTools.Commands.Generate
+{
+    internal class ScriptCommandSetting
+    {
+        public string CommandName { get; set; }
+        public string NameOnRibbon { get; set; }
+        public string ScriptPath { get; set; }
+        public string ParentButton { get; set; }
+        public string ToolTip { get; set; }
+        public string ImageUri { get; set; }
+}
