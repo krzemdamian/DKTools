8ad24ebea7dda2e9106fdd949f0cdabcd6b44de9
Damian Krzeminski <krzemdamian@gmail.com>
2019-01-12 14:03:17 +0100

    Extract interface.

---
+++ DKTools/RevitDKTools/Commands/Generate/ISettingsInterpreter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RevitDKTools.Commands.Generate
+{
+    interface ISettingsInterpreter
+    {
+        ICollection<CommandSetting> ScriptCommandSettings { get; }
+    }
+}
