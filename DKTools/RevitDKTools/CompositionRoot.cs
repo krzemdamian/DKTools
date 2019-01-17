367c2d32ace7422b60a7336f560e52dba6562cac
Damian Krzeminski <krzemdamian@gmail.com>
2019-01-17 14:10:06 +0100

    Moved composition to separete class.

--- DKTools/RevitDKTools/CompositionRoot.cs
+++ DKTools/RevitDKTools/CompositionRoot.cs
@@ -1,4 +1,10 @@
 ﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using RevitDKTools.Commands.Generate;
+using RevitDKTools.Commands.Panels;
+using RevitDKTools.DockablePanels.ParameterEditor.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +16,52 @@ namespace RevitDKTools
     class CompositionRoot : ICompositionRoot
     {
         private UIControlledApplication _application;
+        private ParameterEditorWPFPage _parameterEditorWPFPage;
 
         public CompositionRoot(UIControlledApplication application)
         {
             _application = application;
+
+            PrepareParameterEditorDockablePanel();
         }
-    }
+
+        private void PrepareParameterEditorDockablePanel()
+        {
+            CreateDockablePanel(_application);
+            _application.ViewActivated += new EventHandler
+                <Autodesk.Revit.UI.Events.ViewActivatedEventArgs>(PassRevitDocumentInstance_OnViewActivated);
+            RegisterDocablePanelToRevit(_application);
+        }
+
+        private void PassRevitDocumentInstance_OnViewActivated(object sender, ViewActivatedEventArgs e)
+        {
+            if (_parameterEditorWPFPage.RevitDocument != e.Document)
+            {
+                _parameterEditorWPFPage.RevitDocument = e.Document;
+            }
+        }
+
+        private void CreateDockablePanel(UIControlledApplication application)
+        {
+            _parameterEditorWPFPage = new ParameterEditorWPFPage();
+            _parameterEditorWPFPage.VM.RevitSelectionWatcher = new SelectionChangedWatcher(application);
+            _parameterEditorWPFPage.VM.RevitSelectionWatcher.SelectionChanged +=
+                new EventHandler(_parameterEditorWPFPage.VM.RevitActiveSelection_SelectionChanged);
+            _parameterEditorWPFPage.VM.RevitSelectionWatcher.SelectionChanged +=
+                new EventHandler(_parameterEditorWPFPage.OverwriteContentInRichTextBox);
+        }
+
+        private void RegisterDocablePanelToRevit(UIControlledApplication application)
+        {
+            DockablePaneProviderData dockablePaneProviderData = new DockablePaneProviderData();
+            dockablePaneProviderData.FrameworkElement = _parameterEditorWPFPage as System.Windows.FrameworkElement;
+            dockablePaneProviderData.InitialState = new DockablePaneState();
+            dockablePaneProviderData.InitialState.DockPosition = DockPosition.Bottom;
+            DockablePaneId dpid = new DockablePaneId(
+                new Guid("{F1D5DCB2-DB78-483C-8A77-C7BD7CBC6557}"));
+            application.RegisterDockablePane(
+                dpid, "DKTools: Parameter Editor", _parameterEditorWPFPage
+                as IDockablePaneProvider);
+        }
+   }
 }
--- DKTools/RevitDKTools/DKToolsApp.cs
+++ DKTools/RevitDKTools/DKToolsApp.cs
@@ -22,9 +22,8 @@ namespace RevitDKTools
         public static UIControlledApplication UIControlledApplication { get; set; }
         public static IPythonExecutionEnviroment MyPythonEngine { get; set; }
 
-        private DKToolsApp _DKToolsAppInstance;
-        private ParameterEditorWPFPage _parameterEditorWPFPage;
         private RibbonPanel _commandsRibbonPanel;
+        private WindsorContainer _container;
 
         public Result OnShutdown(UIControlledApplication application)
         {
@@ -33,91 +32,29 @@ namespace RevitDKTools
 
         public Result OnStartup(UIControlledApplication application)
         {
-            AssignProperties(application);
-            InstantiatePythonEngine();
-            CreateButtonsOnRevitRibbon(application);
-            PrepareParameterEditorDockablePanel(application);
-            //CreateDynamicAssemblyAsProxyForPythonScripts();
+            UIControlledApplication = application;
+            MyPythonEngine = new PythonExecutionEnviroment();
 
-            WindsorContainer container = new WindsorContainer();
-            container.Register(Component.For<UIControlledApplication>().Instance(application));
-            container.Register(Component.For<ICompositionRoot>().ImplementedBy<CompositionRoot>());
-            //types for dynamic buttons generation
-            container.Register(Component.For<ICommandsGenerator>().ImplementedBy<CommandsGenerator>());
-            container.Register(Component.For<IClassEmitter>().
-                               ImplementedBy<ClassEmitter<PythonCommandProxyBaseClass>>());
-            container.Register(
-                Component.For<IEmitterSetting,IXmlPythonScriptsSettingsProvider>().
-                ImplementedBy<DefaultSettingsProvider>());
-            container.Register(Component.For<ISettingsInterpreter>().ImplementedBy<XmlSettingsInterpreter>());
-            container.Register(Component.For<RibbonPanel>().Instance(_commandsRibbonPanel).
-                LifestyleSingleton());
-
-            RibbonPanel panel = container.Resolve<RibbonPanel>();
-            ICommandsGenerator generator = container.Resolve<ICommandsGenerator>();
-            generator.GenerateDynamicCommands();
+            RegisterTypesToDIContainer(application);
 
             return Result.Succeeded;
         }
 
-        private void AssignProperties(UIControlledApplication application)
+        private void RegisterTypesToDIContainer(UIControlledApplication application)
         {
-            _DKToolsAppInstance = this;
-            UIControlledApplication = application;
-        }
-
-        private static void InstantiatePythonEngine()
-        {
-            MyPythonEngine = new PythonExecutionEnviroment();
-        }
-
-        private void CreateDynamicAssemblyAsProxyForPythonScripts()
-        {
-            CommandsGenerator generator =
-                new CommandsGenerator (
-                    new ClassEmitter<PythonCommandProxyBaseClass>(new DefaultSettingsProvider()),
-                    new XmlSettingsInterpreter(new DefaultSettingsProvider()),
-                    _commandsRibbonPanel);
-            generator.GenerateDynamicCommands();
-        }
-
-        private void PrepareParameterEditorDockablePanel(UIControlledApplication application)
-        {
-            CreateDockablePanel(application);
-            application.ViewActivated += new EventHandler
-                <Autodesk.Revit.UI.Events.ViewActivatedEventArgs>(PassRevitDocumentInstance_OnViewActivated);
-            RegisterDocablePanelToRevit(application);
-        }
-
-        private void PassRevitDocumentInstance_OnViewActivated(object sender, ViewActivatedEventArgs e)
-        {
-            if (_parameterEditorWPFPage.RevitDocument != e.Document)
-            {
-                _parameterEditorWPFPage.RevitDocument = e.Document;
-            }
-        }
-
-        private void CreateDockablePanel(UIControlledApplication application)
-        {
-            _parameterEditorWPFPage = new ParameterEditorWPFPage();
-            _parameterEditorWPFPage.VM.RevitSelectionWatcher = new SelectionChangedWatcher(application);
-            _parameterEditorWPFPage.VM.RevitSelectionWatcher.SelectionChanged +=
-                new EventHandler(_parameterEditorWPFPage.VM.RevitActiveSelection_SelectionChanged);
-            _parameterEditorWPFPage.VM.RevitSelectionWatcher.SelectionChanged +=
-                new EventHandler(_parameterEditorWPFPage.OverwriteContentInRichTextBox);
-        }
-
-        private void RegisterDocablePanelToRevit(UIControlledApplication application)
-        {
-            DockablePaneProviderData dockablePaneProviderData = new DockablePaneProviderData();
-            dockablePaneProviderData.FrameworkElement = _parameterEditorWPFPage as System.Windows.FrameworkElement;
-            dockablePaneProviderData.InitialState = new DockablePaneState();
-            dockablePaneProviderData.InitialState.DockPosition = DockPosition.Bottom;
-            DockablePaneId dpid = new DockablePaneId(
-                new Guid("{F1D5DCB2-DB78-483C-8A77-C7BD7CBC6557}"));
-            application.RegisterDockablePane(
-                dpid, "DKTools: Parameter Editor", _parameterEditorWPFPage
-                as IDockablePaneProvider);
+            _container = new WindsorContainer();
+            _container.Register(Component.For<UIControlledApplication>().Instance(application));
+            _container.Register(Component.For<ICompositionRoot>().ImplementedBy<CompositionRoot>());
+            //types for dynamic buttons generation
+            _container.Register(Component.For<ICommandsGenerator>().ImplementedBy<CommandsGenerator>());
+            _container.Register(Component.For<IClassEmitter>().
+                               ImplementedBy<ClassEmitter<PythonCommandProxyBaseClass>>());
+            _container.Register(
+                Component.For<IEmitterSetting, IXmlPythonScriptsSettingsProvider>().
+                ImplementedBy<DefaultSettingsProvider>());
+            _container.Register(Component.For<ISettingsInterpreter>().ImplementedBy<XmlSettingsInterpreter>());
+            _container.Register(Component.For<RibbonPanel>().Instance(_commandsRibbonPanel).
+                LifestyleSingleton());
         }
 
         private RibbonPanel CreateButtonsOnRevitRibbon(UIControlledApplication application)
