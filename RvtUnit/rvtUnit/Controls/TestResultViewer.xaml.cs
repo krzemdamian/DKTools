ccdb58133e1c4f2329734e95271e8740d03267e2
Damian Krzeminski <krzemdamian@gmail.com>
2019-02-24 20:53:07 +0100

    Add missing rvtTest files

---
+++ RvtUnit/rvtUnit/Controls/TestResultViewer.xaml.cs
@@ -0,0 +1,55 @@
+﻿#region Copyright (c) 2009-2011 Arup, All rights reserved.
+//
+// All rights are reserved. Reproduction or transmission in whole or in part, in
+// any form or by any means, electronic, mechanical or otherwise, is prohibited
+// without the prior written consent of the copyright owner.
+//
+#endregion
+#region File Information
+//
+// Filename: TestResultViewerViewModel.cs
+// Author: Yamin Tengono <Yamin.Tengono@arup.com.au>
+//
+// This file is part of CADtools Revit - rvtUnit module.
+//
+#endregion
+
+// !!! This file is applicable for all versions of Revit. !!! //
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace rvtUnit.Controls
+{
+   /// <summary>
+   /// Interaction logic for TestResultViewer.xaml
+   /// </summary>
+   public partial class TestResultViewer : Window
+   {
+      public TestResultViewer(TestResultViewerViewModel vm)
+      {
+         InitializeComponent();
+
+         this.DataContext = vm;
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="e"></param>
+      protected override void OnSourceInitialized(EventArgs e)
+      {
+         base.OnSourceInitialized(e);
+      }
+   }
+}
---
+++ RvtUnit/rvtUnit/Controls/TestResultViewerViewModel.cs
@@ -0,0 +1,73 @@
+﻿#region Copyright (c) 2009-2011 Arup, All rights reserved.
+//
+// All rights are reserved. Reproduction or transmission in whole or in part, in
+// any form or by any means, electronic, mechanical or otherwise, is prohibited
+// without the prior written consent of the copyright owner.
+//
+#endregion
+#region File Information
+//
+// Filename: TestResultViewerViewModel.cs
+// Author: Yamin Tengono <Yamin.Tengono@arup.com.au>
+//
+// This file is part of CADtools Revit - rvtUnit module.
+//
+#endregion
+
+// !!! This file is applicable for all versions of Revit. !!! //
+
+using System;
+using System.Collections.ObjectModel;
+using NUnit.Core;
+
+namespace rvtUnit.Controls
+{
+
+   /// <summary>
+   /// Provide data link for TestResultViewer user interface.
+   /// </summary>
+   public class TestResultViewerViewModel
+   {
+
+      /// <summary>
+      /// Creates an instance of TestResultViewerViewModel.
+      /// </summary>
+      /// <param name="rootResult">The root of test result object.</param>
+      public TestResultViewerViewModel(TestResult rootResult, string resultFile)
+      {
+         TestResults = new ObservableCollection<TestItemViewModel>();
+
+         ResultFileName = resultFile;
+
+         WalkTheTestResult(rootResult);
+      }
+
+      /// <summary>
+      /// List of available test results.
+      /// </summary>
+      public ObservableCollection<TestItemViewModel> TestResults { get; set; }
+
+      /// <summary>
+      /// Gets the result file name.
+      /// </summary>
+      public string ResultFileName { get; set; }
+
+      private void WalkTheTestResult(TestResult result)
+      {
+         if (result.Test.TestType.Equals("TestMethod", StringComparison.InvariantCultureIgnoreCase))
+         {
+            TestItemViewModel itemVM = new TestItemViewModel(result);
+            TestResults.Add(itemVM);
+            return;
+         }
+         if (result.Results == null) { return; }
+         if (result.Results.Count <= 0) { return; }
+         foreach (TestResult child in result.Results)
+         {
+            WalkTheTestResult(child);
+         }
+      }
+
+   }  // End of class TestResultViewerViewModel
+
+}  // End of namespace rvtUnit.Controls
