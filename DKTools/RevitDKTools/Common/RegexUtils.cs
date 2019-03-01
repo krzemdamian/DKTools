eb121d10819497c310db451da11843f5256ff04c
Damian Krzeminski <krzemdamian@gmail.com>
2019-03-01 20:31:12 +0100

    Add RegexUtils

---
+++ DKTools/RevitDKTools/Common/RegexUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RevitDKTools.Common
+{
+    static class RegexUtils
+    {
+        private static bool IsValidRegex(string pattern)
+        {
+            if (string.IsNullOrEmpty(pattern)) return false;
+
+            try
+            {
+                Regex.Match("", pattern);
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
